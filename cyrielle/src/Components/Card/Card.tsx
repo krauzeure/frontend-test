48d774ab99bd21753dc71df425a4c58ad5a7d16b
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 21:48:28 +0100

    Updating the component to only send the correct ID to parent element

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -1,33 +1,17 @@
 import React from 'react'
 import '../../css/style.css'
 
-import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from '../..'
+export default function Card(props: {image: string, alt: string, hidden: boolean, returnCard:(id: number) => void, number: number, test: boolean}) {
 
-export default function Card(props: {image: string, alt: string}) {
-
-    const [isHidden, setIsHidden] = useState(true)
-
-    const {cardNames} = useSelector<RootState, {cardNames: string[]}>(state => ({
-        ...state.cardsShownReducer
-      }))
-      const dispatch = useDispatch();
-
-    const returnCard = (e: any) => {
-        setIsHidden(false);
-        let classes = e.target.className
-        let name = classes.split(" ")[1]
-        dispatch({
-            type: "FIRSTCARD",
-            payload: name
-        })
-        console.log(cardNames)
+    const returnCard = () => {
+        props.returnCard(props.number);
     }
+
+    console.log("render")
 
   return (
     <div className={`card-container ${props.alt}`} onClick={returnCard}>
-        <div className={isHidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}></div>
+        <div className={props.hidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}>{props.number}</div>
         <img src={props.image} alt={props.alt} />
     </div>
   )
