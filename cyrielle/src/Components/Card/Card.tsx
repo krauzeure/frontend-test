be6fe58fd69d6b407a851a5dcfd9036ae2fc07af
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 21:49:01 +0100

    Removing the ID number used for testing

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -11,7 +11,7 @@ export default function Card(props: {image: string, alt: string, hidden: boolean
 
   return (
     <div className={`card-container ${props.alt}`} onClick={returnCard}>
-        <div className={props.hidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}>{props.number}</div>
+        <div className={props.hidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}></div>
         <img src={props.image} alt={props.alt} />
     </div>
   )
