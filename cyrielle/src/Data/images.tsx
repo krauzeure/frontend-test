28245cbfc04189f71ca2e79aa72cc9311cb4f6f5
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 15:06:37 +0100

    Creating our image data file

---
+++ cyrielle/src/Data/images.tsx
@@ -0,0 +1,52 @@
+import reactImg from '../Assets/react.png'
+import cssImg from '../Assets/css.png'
+import gitImg from '../Assets/git.png'
+import jsImg from '../Assets/js.png'
+import sassImg from '../Assets/sass.png'
+import htmlImg from '../Assets/html.png'
+import typescriptImg from '../Assets/typescript.png'
+import reduxtImg from '../Assets/redux.png'
+
+export const images = [
+    {
+        "id": 1,
+        "name": "react",
+        "image": reactImg
+    },
+    {
+        "id": 2,
+        "name": "css",
+        "image": cssImg
+    },
+    {
+        "id": 3,
+        "name": "git",
+        "image": gitImg
+    },
+    {
+        "id": 4,
+        "name": "js",
+        "image": jsImg
+    },
+    {
+        "id": 5,
+        "name": "sass",
+        "image": sassImg
+    },
+    {
+        "id": 6,
+        "name": "html",
+        "image": htmlImg
+    },
+    {
+        "id": 7,
+        "name": "typescript",
+        "image": typescriptImg
+    },
+    {
+        "id": 8,
+        "name": "redux",
+        "image": reduxtImg
+    },
+
+]
