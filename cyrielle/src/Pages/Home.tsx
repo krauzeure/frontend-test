d78fac9fe5c4e26d6f5d95a8834eec25fdd79355
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 15:05:54 +0100

    Importing main components on home page

--- cyrielle/src/Pages/Home.tsx
+++ cyrielle/src/Pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import GameBoard from '../Components/GameBoard/GameBoard'
+import Timer from '../Components/Timer/Timer'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main>
+        <section>Explication du jeu</section>
+        <Timer />
+        <GameBoard />
+    </main>
   )
 }
