0a94439b952d124f97276f248df131cea3555f3f
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 16:12:27 +0100

    Fixing the multiple reducers

--- cyrielle/src/redux/store.jsx
+++ cyrielle/src/redux/store.jsx
@@ -1,12 +1,11 @@
-import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
 import correctPairsReducer from "./Reducers/correctPairsReducer";
 import gameStatusReducer from "./Reducers/gameStatusReducer";
 
-const rootReducer = combineReducers({
-    correctPairsReducer,
-    gameStatusReducer
-})
-
-export const store = configureStore(rootReducer);
+export const store = configureStore({
+    reducer:{
+        correctPairsReducer,
+        gameStatusReducer
+     }
+});
