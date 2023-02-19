1893b03fd8bbdf6e0ffca8f058ffb603be377567
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 16:50:57 +0100

    Adding the new reducer to the store

--- cyrielle/src/redux/store.jsx
+++ cyrielle/src/redux/store.jsx
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import correctPairsReducer from "./Reducers/correctPairsReducer";
 import gameStatusReducer from "./Reducers/gameStatusReducer";
+import cardsShownReducer from "./Reducers/cardShownReducer";
 
 export const store = configureStore({
     reducer:{
         correctPairsReducer,
-        gameStatusReducer
+        gameStatusReducer,
+        cardsShownReducer
      }
 });
