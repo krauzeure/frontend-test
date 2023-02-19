ee270c1cf816ef064348345882731766c8da7099
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 16:16:01 +0100

    Creating the state for the card composant and the function to show cards

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '../../css/style.css'
 
+import { useState } from 'react'
+
 export default function Card(props: {image: string, alt: string}) {
+
+    const [isHidden, setIsHidden] = useState(true)
+
+    const returnCard = () => {
+        setIsHidden(false);
+    }
+
   return (
-    <div className={`card-container ${props.alt}`} >
-        <div className="card-cover"></div>
+    <div className={`card-container ${props.alt}`} onClick={returnCard}>
+        <div className={isHidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}></div>
         <img src={props.image} alt={props.alt} />
     </div>
   )
