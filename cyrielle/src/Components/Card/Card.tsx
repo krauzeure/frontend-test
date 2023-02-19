a2bb44387be89a18b151dba58ddca737845a5311
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 15:21:24 +0100

    Creating a shuffled list of cards for the game

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props: {image: string, alt: string}) {
   return (
-    <div>Card</div>
+    <div className={`card-container ${props.alt}`} >
+        <div className="card-cover"></div>
+        <img src={props.image} alt={props.alt} />
+    </div>
   )
 }
--- cyrielle/src/Components/GameBoard/GameBoard.tsx
+++ cyrielle/src/Components/GameBoard/GameBoard.tsx
@@ -3,8 +3,28 @@ import Card from '../Card/Card'
 import { images } from '../../Data/images'
 import { v4 as uuidv4 } from 'uuid'
 
+// Creating an array with 2 instances of each picture
+const allImages = [...images, ...images];
+
+// Creating a function to shuffle our array
+const shuffleArray = (array: { id: number; name: string; image: string; }[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+// Shuffling the array
+shuffleArray(allImages);
+
 export default function GameBoard() {
   return (
-    <ul></ul>
+    <ul>
+        {allImages.map(item => (
+          <Card image={item.image} alt={item.name} key={uuidv4()} />
+        ))}
+    </ul>
   )
 }
