f167997870e395b6e3110d519391a38e91c99eac
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-19 16:56:13 +0100

    Dispatching card name on click

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -2,13 +2,27 @@ import React from 'react'
 import '../../css/style.css'
 
 import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../..'
 
 export default function Card(props: {image: string, alt: string}) {
 
     const [isHidden, setIsHidden] = useState(true)
 
-    const returnCard = () => {
+    const {cardNames} = useSelector<RootState, {cardNames: string[]}>(state => ({
+        ...state.cardsShownReducer
+      }))
+      const dispatch = useDispatch();
+
+    const returnCard = (e: any) => {
         setIsHidden(false);
+        let classes = e.target.className
+        let name = classes.split(" ")[1]
+        dispatch({
+            type: "FIRSTCARD",
+            payload: name
+        })
+        console.log(cardNames)
     }
 
   return (
