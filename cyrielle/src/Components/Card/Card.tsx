dd34dd58c39e21823b857766f410e074480c8140
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-21 09:21:37 +0100

    removing useless prop

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../css/style.css'
 
-export default function Card(props: {image: string, alt: string, hidden: boolean, returnCard:(id: number, cardName: string) => void, number: number, test: boolean}) {
+export default function Card(props: {image: string, alt: string, hidden: boolean, returnCard:(id: number, cardName: string) => void, number: number}) {
 
     const returnCard = () => {
         props.returnCard(props.number, props.alt);
