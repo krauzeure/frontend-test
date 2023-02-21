ba7c99d28c7352cbe10208a6ba49045e92db4ad7
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-21 08:21:11 +0100

    Adding gameboardreducer to the store

--- cyrielle/src/redux/store.jsx
+++ cyrielle/src/redux/store.jsx
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import correctPairsReducer from "./Reducers/correctPairsReducer";
 import gameStatusReducer from "./Reducers/gameStatusReducer";
 import cardsShownReducer from "./Reducers/cardShownReducer";
+import gameBoardReducer from "./Reducers/gameBoardReducer";
 
 export const store = configureStore({
     reducer:{
         correctPairsReducer,
         gameStatusReducer,
-        cardsShownReducer
+        cardsShownReducer,
+        gameBoardReducer
      }
 });
