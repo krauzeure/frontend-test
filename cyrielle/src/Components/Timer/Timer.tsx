08e9cad669819744557f02743f304d2b05b49d60
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-21 15:43:17 +0100

    Adding the setinterval in the componant to update the progress in the store

--- cyrielle/src/Components/Timer/Timer.tsx
+++ cyrielle/src/Components/Timer/Timer.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../index'
+import { useEffect } from 'react'
 
 export default function Timer() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      dispatch({
+        type: "TIME"
+      })
+    }, 5000);
+    return () => clearInterval(timer);
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [])
+
+  const {progress} = useSelector<RootState, {progress: number}>(state => ({
+    ...state.gameStatusReducer
+  }))
+
+  console.log(progress)
+
   return (
     <div>Timer</div>
   )
