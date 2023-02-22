586eaecc6a87466c4c3a311533aaf16397b5dbf1
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-22 17:47:11 +0100

    Adding onclick function test

--- cyrielle/src/Components/Card/Card.test.tsx
+++ cyrielle/src/Components/Card/Card.test.tsx
@@ -1,7 +1,9 @@
 import { render, screen } from "@testing-library/react"
+import user from '@testing-library/user-event'
 import Card from "./Card"
 import { store } from "../../redux/store"
 import { Provider } from "react-redux"
+import userEvent from "@testing-library/user-event"
 
 describe('Card component', () => {
     test('renders correctly', () => {
@@ -12,5 +14,15 @@ describe('Card component', () => {
 
         const imageAlt = screen.getByAltText("test")
         expect(imageAlt).toBeInTheDocument()
+    })
+
+    test('handler is called on click', async () => {
+        const returnFunc = jest.fn()
+
+        render(<Provider store={store}><Card image={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"} alt={"test"} hidden={false} returnCard={returnFunc} number={2} disabled={false} /></Provider>)
+
+        const imageBlock = screen.getByTestId("image-block")
+        await user.click(imageBlock)
+        expect(returnFunc).toHaveBeenCalledTimes(1)
     })
 })
