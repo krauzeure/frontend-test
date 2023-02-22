e1a94e0c98b6b80b64ddf3aa2964ef058c607330
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-22 17:46:50 +0100

    Adding testid to main block

--- cyrielle/src/Components/Card/Card.tsx
+++ cyrielle/src/Components/Card/Card.tsx
@@ -8,7 +8,7 @@ export default function Card(props: {image: string, alt: string, hidden: boolean
     }
 
   return (
-    <div className={`card-container ${props.alt}`} onClick={returnCard}>
+    <div className={`card-container ${props.alt}`} onClick={returnCard} data-testid="image-block">
         <div className={props.hidden ? `card-cover ${props.alt}` : `card-cover-shown ${props.alt}`}></div>
         <img src={props.image} alt={props.alt} />
     </div>
