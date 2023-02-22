f72bda0c45ef66f7c2c60af29d90c25763182ad5
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-22 19:08:30 +0100

    editing the handler test

--- cyrielle/src/Components/Card/Card.test.tsx
+++ cyrielle/src/Components/Card/Card.test.tsx
@@ -20,7 +20,7 @@ describe('Card component', () => {
 
         render(<Provider store={store}><Card image={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"} alt={"test"} hidden={false} returnCard={returnFunc} number={2} disabled={false} /></Provider>)
 
-        const imageBlock = screen.getByTestId("image-block")
+        const imageBlock = screen.getByRole("listitem")
         user.click(imageBlock)
         expect(returnFunc).toHaveBeenCalledTimes(1)
     })
