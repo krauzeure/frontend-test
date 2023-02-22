5cf72b7941373d460f9e4dc31773ca98aeacd075
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-22 19:09:25 +0100

    Adding test on conditional rendering

--- cyrielle/src/Pages/Home.test.tsx
+++ cyrielle/src/Pages/Home.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react"
-
+import { act } from 'react-dom/test-utils';
 import Home from './Home'
 import { store } from "../redux/store"
 import { Provider } from "react-redux"
@@ -16,11 +16,33 @@ describe('Home component', () => {
         const heading = screen.getByRole("heading")
         expect(heading).toHaveTextContent("Bienvenue sur le jeu Memory")
 
-        const winMessage = screen.queryByText('Votre temps est écoulé ! Cliquez sur "Démarrer" pour relancer une partie.')
+        const winMessage = screen.queryByText('Félicitations 🙌 ! Cliquez sur "Démarrer" pour relancer une partie.')
         expect(winMessage).toBeNull()
 
-        const lossMessage = screen.queryByText('Félicitations 🙌 ! Cliquez sur "Démarrer" pour relancer une partie.')
+        const lossMessage = screen.queryByText('Votre temps est écoulé ! Cliquez sur "Démarrer" pour relancer une partie.')
         expect(lossMessage).toBeNull()
+    })
+
+    test('shows error message when game is lost', async () => {
+        render(
+            <Provider store={store}>
+                <Home/>
+            </Provider>)
+
+            await act(() => store.dispatch({type: "TIMEOVER"}));
+            const lossMessage = screen.queryByText('Votre temps est écoulé ! Cliquez sur "Démarrer" pour relancer une partie.')
+            expect(lossMessage).toBeInTheDocument()
+    })
+
+    test('shows victory message when game is won', async () => {
+        render(
+            <Provider store={store}>
+                <Home/>
+            </Provider>)
+
+            await act(() => store.dispatch({type: "WON"}));
+            const winMessage = screen.queryByText('Félicitations 🙌 ! Cliquez sur "Démarrer" pour relancer une partie.')
+            expect(winMessage).toBeInTheDocument()
     })
 
 })
